bb5f99ccf3bcc7cd06b35ba86a1aa2d3fdf3428c
Tony John <tonyjanugrah@gmail.com>
2024-02-20 18:59:19 +0100

    add 'net' and 'ix' to iface filter

--- pconn_manager.go
+++ pconn_manager.go
@@ -200,7 +200,9 @@ func (pcm *pconnManager) createPconns() error {
 	}
 	for _, i := range ifaces {
 		// TODO (QDC): do this in a generic way
-		if !strings.Contains(i.Name, "eth") && !strings.Contains(i.Name, "rmnet") && !strings.Contains(i.Name, "wlan") {
+		if !strings.Contains(i.Name, "eth") && !strings.Contains(i.Name, "rmnet") &&
+			!strings.Contains(i.Name, "wlan") && !strings.Contains(i.Name, "net") &&
+			!strings.Contains(i.Name, "ix") {
 			continue
 		}
 		addrs, err := i.Addrs()
